ce0e9a5a46677e0e184c4a456290fec924b3263b
UpdateEDK <119038046+UpdateEDK@users.noreply.github.com>
2022-11-25 17:12:36 +0800

    final

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta http-equiv="refresh" content="3;url=https://discord.gg/QgwRgprDpn" />
+    </head>
+    <body>
+        <h1>Redirecting in 3 seconds...</h1>
+    </body>
+</html>
